8d718b88b1fb2af56d42772b4c109b9a522dab4d
wnsgh12s <junho1273@gmail.com>
2023-02-28 13:16:23 +0900

    백준 10905 문제

---
+++ 10905.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const [n, ...input] = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+
+input.forEach(e => {
+  console.log(e.split(" ").reduce((a, b) => Number(a) + Number(b)))
+})
